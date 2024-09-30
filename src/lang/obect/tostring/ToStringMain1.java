c91fd7cd318ec7cd47d270a1867ccc314b6c08e9
Hxxseung <soaorsith2@naver.com>
2024-09-30 14:22:47 +0900

    toString && ObjectPrinter && HexString

---
+++ src/lang/obect/tostring/ToStringMain1.java
@@ -0,0 +1,14 @@
+package lang.obect.tostring;
+
+public class ToStringMain1 {
+    public static void main(String[] args) {
+        Object object = new Object();
+        String string = new String();
+
+        //toString() 반환값 출력
+        System.out.println(string);
+
+        //object 직접 출력
+        System.out.println(object);
+    }
+}
