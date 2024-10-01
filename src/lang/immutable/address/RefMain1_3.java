2fd2efa8712eed36d1f3e7bf3b8172c3fd004792
Hxxseung <soaorsith2@naver.com>
2024-10-01 14:51:42 +0900

    Immutable && Mutable

---
+++ src/lang/immutable/address/RefMain1_3.java
@@ -0,0 +1,21 @@
+package lang.immutable.address;
+
+public class RefMain1_3 {
+    public static void main(String[] args) {
+
+        //참조형 변수는 하나의 인스턴스를 공유할 수 있다.
+        Address a = new Address("서울");  //새로운 객체 참조
+        Address b = a;  //기존 객체 공유 참조
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        change(b,"부산");   //b의 값을 부산으로 변경해야함.
+        System.out.println("부산 -> b");
+        System.out.println("a = " + a); //사이드 이펙트 발생
+        System.out.println("b = " + b);
+    }
+    public static void change(Address address, String changeAddress) {
+        System.out.println("주소 값을 변경합니다 -> " + changeAddress);
+        address.setValue(changeAddress);
+    }
+}
